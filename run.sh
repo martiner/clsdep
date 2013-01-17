b4173f9147004e3e52436fa804df0c0d063df23f
Martin Caslavsky <martin.caslavsky@gooddata.com>
2013-01-17 17:18:12 +0100

    add resource file

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -e -x
+
 TARGET=target
 MAINJAR=$TARGET/main.jar
 DEPENDJAR=$TARGET/depend.jar
+SRC=src
 
 mkdir -p $TARGET
-javac -d $TARGET src/Main.java src/Depend.java
+javac -d $TARGET $SRC/Main.java $SRC/Depend.java
 
 jar cf $MAINJAR -C $TARGET Main.class
-jar cf $DEPENDJAR -C $TARGET Depend.class
+jar cf $DEPENDJAR -C $TARGET Depend.class -C $SRC foo.txt
 
 java -cp $MAINJAR:$DEPENDJAR Main
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String... args) throws Exception {
+	public static void main(String... args) throws Exception {
 		try {
 			Class.forName("FOO").newInstance();
 		} catch (Exception ignore) {}
@@ -10,6 +10,8 @@ public class Main {
 		System.out.print("Delete the target directory then hit ENTER: ");
 		new Scanner(System.in).nextLine();
 
-		Class.forName("Depend").newInstance();
+		Object depend = Class.forName("Depend").newInstance();
+		System.out.println(depend.getClass().getResource("/foo.txt"));
+
 	}
 }
